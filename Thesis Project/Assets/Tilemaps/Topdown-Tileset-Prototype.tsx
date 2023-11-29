1ed8d7c7a17ff4a838b06d1c2fbf4c80cb9e70a9
Ziggy-dst <542335894@qq.com>
2023-11-29 11:43:11 -0500

    First Commit, Set up Tiled

---
+++ Thesis Project/Assets/Tilemaps/Topdown-Tileset-Prototype.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Topdown-Tileset-Prototype" tilewidth="16" tileheight="16" tilecount="45" columns="9">
+ <image source="Tileset/Prototype-Topdown-Tileset.png" width="144" height="80"/>
+</tileset>
