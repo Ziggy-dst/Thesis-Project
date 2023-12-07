d35cb184cfadec79e8acb39347db147a28ad8b9a
blaerzhang <zhangyueyang0022@126.com>
2023-12-07 17:35:43 -0500

    Player dodging behavior added

--- Thesis Project/Assets/Scripts/FixNavMeshAgentRotation.cs
+++ Thesis Project/Assets/Scripts/FixNavMeshAgentRotation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,5 +10,11 @@ public class FixNavMeshAgentRotation : MonoBehaviour
 		var agent = GetComponent<NavMeshAgent>();
 		agent.updateRotation = false;
 		agent.updateUpAxis = false;
+	}
+
+	private void Update()
+	{
+		Vector3 pos3D = transform.position;
+		transform.position = new Vector3(pos3D.x, pos3D.y, 0);
 	}
 }
