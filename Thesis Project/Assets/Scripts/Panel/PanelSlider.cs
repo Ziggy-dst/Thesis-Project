a7d1f677711077e63b35268e5a70fa7a23ff207d
Ziggy-dst <542335894@qq.com>
2023-12-14 16:04:44 -0500

    Added slider functions

--- Thesis Project/Assets/Scripts/Panel/PanelSlider.cs
+++ Thesis Project/Assets/Scripts/Panel/PanelSlider.cs
@@ -4,13 +4,33 @@ using UnityEngine;
 
 public class PanelSlider : PanelUIElements
 {
-    public GameObject otherSlider;
+    [HideInInspector] public bool isDragging = false;
+    public float stopDistance = 0.1f;
+    public float maxY = 3.5f;
+    public float minY = -1.5f;
+
+    [HideInInspector] public float percentage = 0;
+
     protected override void MouseDragAction()
     {
+        isDragging = true;
         float mouseYDelta = Input.GetAxis("Mouse Y") * Time.deltaTime;
-        transform.Translate(0, mouseYDelta, 0);
+        Vector3 newPosition = transform.localPosition + new Vector3(0, mouseYDelta, 0);
+        newPosition.y = Mathf.Clamp(Mathf.Round(newPosition.y * 100f) / 100f, minY, maxY);
 
-        // 另一个滑块需要以相同速度反方向移动
-        otherSlider.transform.Translate(0, -mouseYDelta, 0);
+        SetSliderPosition(newPosition.y);
+    }
+
+    protected override void OnMouseUp()
+    {
+        base.OnMouseUp();
+        isDragging = false;
+    }
+
+    public void SetSliderPosition(float newY)
+    {
+        transform.localPosition = new Vector3(transform.localPosition.x, newY, transform.localPosition.z);
+        percentage = (newY - minY) / (maxY - minY);
+        // print("percentage " + gameObject.name + ": " + percentage);
     }
 }
--- Thesis Project/Assets/Scripts/Panel/SliderManager.cs
+++ Thesis Project/Assets/Scripts/Panel/SliderManager.cs
@@ -1,75 +1,87 @@
+using System;
 using UnityEngine;
 
 public class SliderManager : MonoBehaviour
 {
-    public GameObject sliderA;
-    public GameObject sliderB;
-    public float hoverScaleIncrease = 0.1f; // 鼠标悬停时的缩放增加量
-    private Vector3 sliderAOriginalScale;
-    private Vector3 sliderBOriginalScale;
-    private GameObject activeSlider = null; // 当前活动的滑块
+    public PanelSlider sliderA;
+    public PanelSlider sliderB;
+    // public float hoverScaleIncrease = 0.1f; // 鼠标悬停时的缩放增加量
+    // private Vector3 sliderAOriginalScale;
+    // private Vector3 sliderBOriginalScale;
+    // private GameObject activeSlider = null; // 当前活动的滑块
 
-    private void Start()
-    {
-        // 保存原始的缩放比例
-        sliderAOriginalScale = sliderA.transform.localScale;
-        sliderBOriginalScale = sliderB.transform.localScale;
-    }
 
     private void Update()
     {
-        if (!activeSlider)
+        if (!Cursor.visible)
         {
-            CheckForHover(sliderA);
-            CheckForHover(sliderB);
-        }
-
-        if (activeSlider && Input.GetMouseButtonUp(0))
-        {
-            // 松开鼠标左键，退出拖拽状态，显示cursor
-            activeSlider = null;
-            Cursor.visible = true;
-        }
-
-        if (activeSlider)
-        {
-            DragSlider(activeSlider);
+            if (sliderA.isDragging)
+                sliderB.transform.localPosition = new Vector3(sliderB.transform.localPosition.x,  (1 - sliderA.percentage) * (sliderB.maxY - sliderB.minY) + sliderB.minY, 0);
+            if (sliderB.isDragging)
+                sliderA.transform.localPosition = new Vector3(sliderA.transform.localPosition.x,  (1 - sliderB.percentage) * (sliderA.maxY - sliderA.minY) + sliderA.minY, 0);
         }
     }
-
-    private void CheckForHover(GameObject slider)
-    {
-        Collider2D collider = slider.GetComponent<Collider2D>();
-        if (collider.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
-        {
-            // 鼠标悬停在滑块上
-            if (!activeSlider)
-            {
-                // 只有在不拖拽的状态下才进行缩放
-                slider.transform.localScale = sliderAOriginalScale * (1 + hoverScaleIncrease);
-            }
-
-            if (Input.GetMouseButtonDown(0))
-            {
-                // 按下鼠标左键，进入拖拽状态，隐藏cursor
-                activeSlider = slider;
-                Cursor.visible = false;
-            }
-        }
-        else
-        {
-            // 鼠标不悬停在滑块上
-            slider.transform.localScale = slider == sliderA ? sliderAOriginalScale : sliderBOriginalScale;
-        }
-    }
-
-    private void DragSlider(GameObject slider)
-    {
-        float mouseYDelta = Input.GetAxis("Mouse Y") * Time.deltaTime;
-        slider.transform.Translate(0, mouseYDelta, 0);
-
-        // 另一个滑块需要以相同速度反方向移动
-        GameObject otherSlider = slider == sliderA ? sliderB : sliderA;
-        otherSlider.transform.Translate(0, -mouseYDelta, 0);
-    }
+    // private void Start()
+    // {
+    //     // 保存原始的缩放比例
+    //     sliderAOriginalScale = sliderA.transform.localScale;
+    //     sliderBOriginalScale = sliderB.transform.localScale;
+    // }
+    //
+    // private void Update()
+    // {
+    //     if (!activeSlider)
+    //     {
+    //         CheckForHover(sliderA);
+    //         CheckForHover(sliderB);
+    //     }
+    //
+    //     if (activeSlider && Input.GetMouseButtonUp(0))
+    //     {
+    //         // 松开鼠标左键，退出拖拽状态，显示cursor
+    //         activeSlider = null;
+    //         Cursor.visible = true;
+    //     }
+    //
+    //     if (activeSlider)
+    //     {
+    //         DragSlider(activeSlider);
+    //     }
+    // }
+    //
+    // private void CheckForHover(GameObject slider)
+    // {
+    //     Collider2D collider = slider.GetComponent<Collider2D>();
+    //     if (collider.OverlapPoint(Camera.main.ScreenToWorldPoint(Input.mousePosition)))
+    //     {
+    //         // 鼠标悬停在滑块上
+    //         if (!activeSlider)
+    //         {
+    //             // 只有在不拖拽的状态下才进行缩放
+    //             slider.transform.localScale = sliderAOriginalScale * (1 + hoverScaleIncrease);
+    //         }
+    //
+    //         if (Input.GetMouseButtonDown(0))
+    //         {
+    //             // 按下鼠标左键，进入拖拽状态，隐藏cursor
+    //             activeSlider = slider;
+    //             Cursor.visible = false;
+    //         }
+    //     }
+    //     else
+    //     {
+    //         // 鼠标不悬停在滑块上
+    //         slider.transform.localScale = slider == sliderA ? sliderAOriginalScale : sliderBOriginalScale;
+    //     }
+    // }
+    //
+    // private void DragSlider(GameObject slider)
+    // {
+    //     float mouseYDelta = Input.GetAxis("Mouse Y") * Time.deltaTime;
+    //     slider.transform.Translate(0, mouseYDelta, 0);
+    //
+    //     // 另一个滑块需要以相同速度反方向移动
+    //     GameObject otherSlider = slider == sliderA ? sliderB : sliderA;
+    //     otherSlider.transform.Translate(0, -mouseYDelta, 0);
+    // }
 }
